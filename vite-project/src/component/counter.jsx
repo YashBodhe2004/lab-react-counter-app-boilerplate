e26a5dfafff916440f5e0a4b1390763c8658ddd7
YashBodhe2004 <yashbodhe2004@gmail.com>
2024-01-08 11:03:09 +0530

    done

---
+++ vite-project/src/component/counter.jsx
@@ -0,0 +1,29 @@
+import { Component } from "react";
+
+export default class Counter extends Component{
+
+    constructor(){
+        super();
+
+        this.state = {
+            counter : 0
+        }
+    }
+
+    handleClick = (value) =>{
+        this.setState({
+            counter : this.state.counter + value
+        })
+    }
+
+    render(){
+        return(
+            <> 
+                <h1>Counter using Class : { this.state.counter} </h1>
+                <button onClick={ () => {this.handleClick(1)}}> + (Increase)</button>
+                <button onClick={ () => {this.handleClick(-1)}}> - (Decrease)</button>
+                <button onClick={ () => {this.handleReset(  )}}> + Reset </button>
+            </>
+        )
+    }
+}
